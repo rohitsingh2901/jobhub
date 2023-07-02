0a55461c57491d8ded74d4e78e78936ce6092d88
rohitsingh2901 <80673974+rohitsingh2901@users.noreply.github.com>
2023-07-02 17:06:09 +0530

    style in login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,46 @@
 import React from "react";
 
 const Login = () => {
+
   return (
     <div id="loginDiv">
       <div className="container pt-40" id="containerLogin">
         <h1 className="font-extrabold">
-          Register and apply for <span className="sp">FREE</span>
+          Register and apply for <span className="sp" id="typewriter">FREE</span>
         </h1>
         <h4 className="font-bold my-6">
           <span className="sp">1,50,000+ </span> companies hiring on JonHub{" "}
         </h4>
-        <div class="card my-4" style={{"max-width": "440px","max-height": "540px","padding" : "10px"}}>
-  <div class="row no-gutters">
-    <div class="col-md-12">
-      <div class="card-body">
+        <div className="card my-4" id="cardDiv">
+  <div className="row no-gutters">
+    <div className="col-md-12">
+      <div className="card-body">
         <form>
-          <div class="form-group ">
-            <label className="" for="exampleInputEmail1">Email address</label>
+          <div className="form-group ">
+            <label className="" htmlfor="exampleInputEmail1">Email address</label>
             <input
               type="email"
-              class="form-control mt-1"
+              className="form-control mt-1"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="john@example.com"
             />
           </div>
-          <div class="form-group my-3">
-            <label className="" for="exampleInputPassword1">Password</label>
+          <div className="form-group my-3">
+            <label className="" htmlfor="exampleInputPassword1">Password</label>
             <input
               type="password"
-              class="form-control mt-1"
+              className="form-control mt-1"
               id="exampleInputPassword1"
               placeholder="Must be atleast 6 characters"
             />
           </div>
           <div id="fnln">
-          <div class="form-group my-2">
-            <label className="" for="exampleInputName1">First Name</label>
+          <div className="form-group my-2">
+            <label className="" htmlfor="exampleInputName1">First Name</label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="exampleInputName1"
               placeholder="John"
             />
@@ -48,11 +49,11 @@ const Login = () => {
             <p>&nbsp;&nbsp;&nbsp;</p>
           </div>
 
-          <div class="form-group">
-            <label className="" for="exampleInputName1">Last Name</label>
+          <div className="form-group">
+            <label className="" htmlfor="exampleInputName1">Last Name</label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="exampleInputName1"
               placeholder="Doe"
             />
@@ -64,7 +65,7 @@ const Login = () => {
             </small>
             </div>
             <div>
-          <button type="submit" class="btn btn-primary mt-2">
+          <button type="submit" className="btn btn-primary mt-2">
             Submit
           </button>
             </div>
