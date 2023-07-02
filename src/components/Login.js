9f4d31b58be1bd7dc44ffa687502819b2f4b2a5e
rohitsingh2901 <80673974+rohitsingh2901@users.noreply.github.com>
2023-07-02 11:58:35 +0530

    Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='pt-64'>
-      <h1 className='text-center'>This is Login</h1>
-    </div>
-  )
-}
+    <div id="loginDiv">
+      <div className="container pt-40" id="containerLogin">
+        <h1 className="font-extrabold">
+          Register and apply for <span className="sp">FREE</span>
+        </h1>
+        <h4 className="font-bold my-6">
+          <span className="sp">1,50,000+ </span> companies hiring on JonHub{" "}
+        </h4>
+        <div class="card my-4" style={{"max-width": "440px","max-height": "540px","padding" : "10px"}}>
+  <div class="row no-gutters">
+    <div class="col-md-12">
+      <div class="card-body">
+        <form>
+          <div class="form-group ">
+            <label className="" for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control mt-1"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="john@example.com"
+            />
+          </div>
+          <div class="form-group my-3">
+            <label className="" for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control mt-1"
+              id="exampleInputPassword1"
+              placeholder="Must be atleast 6 characters"
+            />
+          </div>
+          <div id="fnln">
+          <div class="form-group my-2">
+            <label className="" for="exampleInputName1">First Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputName1"
+              placeholder="John"
+            />
+          </div>
+          <div>
+            <p>&nbsp;&nbsp;&nbsp;</p>
+          </div>
 
-export default Login
+          <div class="form-group">
+            <label className="" for="exampleInputName1">Last Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputName1"
+              placeholder="Doe"
+            />
+          </div>
+          </div>
+            <div className="my-1">
+          <small className="form-text text-muted">
+            By signing up, you agree to our Terms and Conditions.
+            </small>
+            </div>
+            <div>
+          <button type="submit" class="btn btn-primary mt-2">
+            Submit
+          </button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <nav class="navbar fixed-top navbar-expand-lg anim" id="navbar">
       <a class="navbar-brand" href="/">
         <div class="enlarge-image">
-          <img class="h-20" src="job.png" alt="logo" />
+          <img style={{"height":"4vw"}} src="job.png" alt="logo" />
         </div>
       </a>
       <button
