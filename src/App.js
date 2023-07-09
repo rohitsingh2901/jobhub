950b35642de5226a2a10de756ca2c849878a2641
rohitsingh2901 <80673974+rohitsingh2901@users.noreply.github.com>
2023-07-10 00:47:55 +0530

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import Register from "./components/Register";
 import Contact from "./components/Contact";
 import Jobs from "./components/Jobs";
